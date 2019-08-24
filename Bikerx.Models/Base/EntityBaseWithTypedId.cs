e7b184d55caef14ed8c266546ac21c0941b25332
Victor Latriglia <creappsdeveloper02@outlook.com>
2019-08-24 10:55:14 -0500

    Modelo base

---
+++ Bikerx.Models/Base/EntityBaseWithTypedId.cs
@@ -0,0 +1,16 @@
+﻿using Bikerx.Models.Base;
+
+namespace Bikerx.Models
+{
+    /// <summary>
+    /// Base class for the implementation of models
+    /// </summary>
+    /// <typeparam name="Tid">Property that identified this Class</typeparam>
+    public abstract class EntityBaseWithTypedId<TId> :  IEntityWithTypedId<TId>
+    {
+        /// <summary>
+        /// property that identifies the class
+        /// </summary>
+        public virtual TId Id { get; set; }
+    }
+}
---
+++ Bikerx.Models/Ciclista/Ciclista.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bikerx.Models.Ciclista
+{
+    public class Ciclista : EntityBase<int>
+    {
+        //Propiedades
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public long NumeroContacto { get; set; }
+        public string Email { get; set; }
+        public int Puntos { get; set; }
+        //
+
+        //ForeignKeys
+        public int AmigoId { get; set; }
+        public Ciclista Amigo { get; set; }
+        //
+
+        //Entidades Dependientes
+        public List<Ciclista> Amigos { get; set; }
+        //
+    }
+}
