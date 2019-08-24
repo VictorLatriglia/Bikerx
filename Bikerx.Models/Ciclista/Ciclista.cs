6c9011828a02cbd4e78e1feeda762949064d27a9
Victor Latriglia <creappsdeveloper02@outlook.com>
2019-08-24 11:26:59 -0500

    Ciclistas

--- Bikerx.Models/Ciclista/Ciclista.cs
+++ Bikerx.Models/Ciclista/Ciclista.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Bikerx.Models.Ciclista
 {
+    /// <summary>
+    /// Clase para ciclistas
+    /// </summary>
     public class Ciclista : EntityBase<int>
     {
         //Propiedades
